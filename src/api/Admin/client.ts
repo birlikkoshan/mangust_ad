1813a1fb4c3799a26ea4f1a5862538643ce5e3c9
birlikkoshan <birlikkoshan14@gmail.com>
2026-01-31 01:03:33 +0500

    vercel conflict fixes

--- src/api/Admin/client.ts
+++ src/api/Admin/client.ts
@@ -80,7 +80,7 @@ apiClient.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem("access_token");
       localStorage.removeItem("user");
-      window.location.href = "/login";
+      // window.location.href = "/login";
     }
     return Promise.reject(error);
   },
